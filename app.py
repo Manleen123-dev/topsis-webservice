035cd53b7665a59611e9b346629e6a0524b9f8d4
Manleen123-dev <kaurmanleen05@gmail.com>
2026-01-20 01:18:38 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from topsis import topsis
 
 app = Flask(__name__)
+CORS(app)   # ✅ THIS LINE IS IMPORTANT
 
 @app.route("/")
 def home():
-    return "TOPSIS web service running"
+    return "TOPSIS backend running"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json()
+    data = request.json
+    matrix = data["matrix"]
+    weights = data["weights"]
+    impacts = data["impacts"]
+    labels = data["labels"]
 
-    if not data:
-        return jsonify({"error": "No input data provided"}), 400
+    scores, ranking = topsis(matrix, weights, impacts, labels)
 
-    try:
-        matrix = data["matrix"]
-        weights = data["weights"]
-        impacts = data["impacts"]
-        labels = data.get("labels")
-
-        scores, ranking = topsis(matrix, weights, impacts, labels)
-
-        return jsonify({
-            "scores": scores,
-            "ranking": ranking
-        })
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+    return jsonify({
+        "scores": scores,
+        "ranking": ranking
+    })
 
 if __name__ == "__main__":
     app.run()
+
